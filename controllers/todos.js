3e5a7e1cbd266a9beb69c694e2deb2f05950832b
unknown <johnzak80@gmail.com>
2022-07-25 18:38:18 -0400

    adding multiple inputs and populate in DOM using iteration on the controller

--- controllers/todos.js
+++ controllers/todos.js
@@ -11,50 +11,53 @@ module.exports = {
         }
     },
     createTodo: async (req, res)=>{
+        console.log(req.body.todoItem)
         try{
-            await Todo.create({todo: req.body.todoItem, completed: false})
+            for(let i = 0; i < (req.body.todoItem.length); i++ ){
+            await Todo.create({todo: req.body.todoItem[i], completed: false})}
             console.log('Todo has been added!')
             res.redirect('/todos')
+               
         }catch(err){
             console.log(err)
         }
     },
-    createTodoTwo: async (req, res)=>{
-        try{
-            await Todo.create({todo: req.body.todoItem, completed: false})
-            console.log(req.body.todoItem)
-            console.log('Todo has been added!')
-            res.redirect('/todos')
+    // createTodoTwo: async (req, res)=>{
+    //     try{
+    //         await Todo.create({todo: req.body.todoItem, completed: false})
+    //         console.log(req.body.todoItem)
+    //         console.log('Todo has been added!')
+    //         res.redirect('/todos')
             
-        }catch(err){
-            console.log(err)
-        }
-    },
-    createTodoThree: async (req, res)=>{
-        try{
-            await Todo.create({todo: req.body.todoItem, completed: false})
-            console.log('Todo has been added!')
-            res.redirect('/todos')
-        }catch(err){
-            console.log(err)
-        }
-    },createTodoFour: async (req, res)=>{
-        try{
-            await Todo.create({todo: req.body.todoItem, completed: false})
-            console.log('Todo has been added!')
-            res.redirect('/todos')
-        }catch(err){
-            console.log(err)
-        }
-    },createTodoFive: async (req, res)=>{
-        try{
-            await Todo.create({todo: req.body.todoItem, completed: false})
-            console.log('Todo has been added!')
-            res.redirect('/todos')
-        }catch(err){
-            console.log(err)
-        }
-    },
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // },
+    // createTodoThree: async (req, res)=>{
+    //     try{
+    //         await Todo.create({todo: req.body.todoItem, completed: false})
+    //         console.log('Todo has been added!')
+    //         res.redirect('/todos')
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // },createTodoFour: async (req, res)=>{
+    //     try{
+    //         await Todo.create({todo: req.body.todoItem, completed: false})
+    //         console.log('Todo has been added!')
+    //         res.redirect('/todos')
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // },createTodoFive: async (req, res)=>{
+    //     try{
+    //         await Todo.create({todo: req.body.todoItem, completed: false})
+    //         console.log('Todo has been added!')
+    //         res.redirect('/todos')
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // },
     markComplete: async (req, res)=>{
         try{
             await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
